e2d4f99c61a1c45cc21c09bf44e840da2580d758
PoChuanSu <peterpeter1010@live.com>
2024-10-09 16:22:11 +1100

    async/await practice

--- Program.cs
+++ Program.cs
@@ -16,113 +16,34 @@ namespace HelloWorld
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-        
-            IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-            DataContextDapper dapper = new DataContextDapper(config);
-            // Computer myComputer = new Computer() 
-            // {
-            //     Motherboard = "Z690",
-            //     HasWifi = true,
-            //     HasLTE = false,
-            //     ReleaseDate = DateTime.Now,
-            //     Price = 943.87m,
-            //     VideoCard = "RTX 2060"
-            // };
+            Task firstTask = new Task(() => {
+                Thread.Sleep(100);
+                Console.WriteLine("Task 1");
+            });
+            firstTask.Start();
 
-            // string sql = @"INSERT INTO TutorialAppSchema.Computer (
-            //     Motherboard,
-            //     HasWifi,
-            //     HasLTE,
-            //     ReleaseDate,
-            //     Price,
-            //     VideoCard
-            // ) VALUES ('" + myComputer.Motherboard 
-            //         + "','" + myComputer.HasWifi
-            //         + "','" + myComputer.HasLTE
-            //         + "','" + myComputer.ReleaseDate
-            //         + "','" + myComputer.Price
-            //         + "','" + myComputer.VideoCard + "')";
+            Task secondTask = ConsoleAfterDelayAsync("Task 2", 150);
+            ConsoleAfterDelay("Delay", 75);
 
-            // // File.WriteAllText("log.txt", "\n" + sql + "\n");
-            // using StreamWriter openFile = new("log.txt", append: true);
-            
-            // openFile.WriteLine("\n" + sql + "\n");
+            Task thirdTask = ConsoleAfterDelayAsync("Task 3", 50);
 
-            // openFile.Close();
-            // string fileText = File.ReadAllText("log.txt");
-
-            // Console.WriteLine(fileText);
-
-            string computersJson = File.ReadAllText("ComputersSnake.json");
-
-            Mapper mapper = new Mapper(new MapperConfiguration((cfg) => {
-                cfg.CreateMap<ComputerSnake, Computer>()
-                .ForMember(destination => destination.ComputerId, options =>
-                    options.MapFrom(source => source.computer_id))
-                .ForMember(destination => destination.Motherboard, options =>
-                    options.MapFrom(source => source.motherboard))
-                .ForMember(destination => destination.CPUCores, options =>
-                    options.MapFrom(source => source.cpu_cores))
-                .ForMember(destination => destination.HasWifi, options =>
-                    options.MapFrom(source => source.has_wifi))   
-                .ForMember(destination => destination.HasLTE, options =>
-                    options.MapFrom(source => source.has_lte))
-                .ForMember(destination => destination.ReleaseDate, options =>
-                    options.MapFrom(source => source.release_date))
-                .ForMember(destination => destination.Price, options =>
-                    options.MapFrom(source => source.price))
-                .ForMember(destination => destination.VideoCard, options =>
-                    options.MapFrom(source => source.video_card));
-            }));
-
-            IEnumerable<ComputerSnake>? computersystem = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<ComputerSnake>>(computersJson);
-
-            if (computersystem != null) {
-                IEnumerable<Computer> computerResult = mapper.Map<IEnumerable<Computer>>(computersystem);
-                  Console.WriteLine("Automapper Count: " + computerResult.Count());
-                // foreach (Computer computer in computerResult)
-                // {
-                //     Console.WriteLine(computer.Motherboard);
-                // }
-            }
-
-
-            // Console.WriteLine(computersJson);
-            // JsonSerializerOptions options = new JsonSerializerOptions()
-            // {
-            //     PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-            // };
-
-            // IEnumerable<Computer>? computersNewtonSoft = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<Computer>>(computersJson, options);
-            IEnumerable<Computer>? computersSystemJsonPropertyMappiung = JsonConvert.DeserializeObject<IEnumerable<Computer>>(computersJson);
-
-            if (computersSystemJsonPropertyMappiung != null)
-            {   
-                Console.WriteLine("JSON Property Count: " + computersSystemJsonPropertyMappiung.Count());
-                // foreach (Computer computer in computersSystemJsonPropertyMappiung)
-                // {   
-                //     Console.WriteLine(computer.Motherboard);
-                // }
-            }
-
-            // JsonSerializerSettings settings = new JsonSerializerSettings()
-            // {
-            //     ContractResolver = new CamelCasePropertyNamesContractResolver()
-            // };
-
-            // string computersCopy = JsonConvert.SerializeObject(computersNewtonSoft, settings);
-            // File.WriteAllText("computersCopyNewtonsoft.txt", computersCopy);
-
-            // string computersCopySystem = System.Text.Json.JsonSerializer.Serialize(computersNewtonSoft, options);
-            // File.WriteAllText(" computersCopySystem.txt", computersCopySystem);
+            await firstTask;
+            await secondTask;
+            await thirdTask;
+            Console.WriteLine("After the Task was created");
         }
-        
-        static string EscapeSingleQuote (string input)
+
+        static void ConsoleAfterDelay(string text, int delayTime)
         {
-            string output = input.Replace("'","''");
-            return output;
+            Thread.Sleep(delayTime);
+            Console.WriteLine(text);
+        }
+        static async Task ConsoleAfterDelayAsync (string text, int delayTime)
+        {
+            await Task.Delay(delayTime);
+            Console.WriteLine(text);
         }
     }
 }
