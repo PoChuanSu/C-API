e51e271a2224782c47b40f5798dd970f7466e178
PoChuanSu <peterpeter1010@live.com>
2024-10-16 22:48:41 +1100

    database connection

--- DotnetAPI/Controllers/UserController.cs
+++ DotnetAPI/Controllers/UserController.cs
@@ -5,10 +5,22 @@ namespace DotnetAPI.Controllers;
 [Route("[controller]")]
 public class UserController : ControllerBase
 {
-    public UserController()
-    {
 
+    DataContextDapper _dapper;
+
+    public UserController(IConfiguration config)
+    {
+        _dapper = new DataContextDapper(config);
+        // Console.WriteLine(config.GetConnectionString("DefaultConnection"));
     }
+
+    [HttpGet("TestConnection")]
+
+    public DateTime Testconnection()
+    {
+        return _dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
+    }
+
 
     [HttpGet("GetUsers/{testValue}")]
     public string[] GetUsers(string testValue)
